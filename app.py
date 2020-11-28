775195a7c650b7b0b37516a1c49144b9177f649b
Betty <bettylin0823@hotmail.com>
2020-11-28 11:23:52 +0800

    update

--- app.py
+++ app.py
@@ -46,6 +46,11 @@ def handle_message(event):
             sticker_id='1'
         )
 
+        line_bot_api.reply_message(
+        event.reply_token,
+        sticker_message)
+        return
+
     if msg in ['hi', 'Hi']:
         r = 'hi'
     elif msg == '你吃飯了沒':
